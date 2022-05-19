cbb589576118b61957a8aba8a420dfb70700f53f
André Rosa <25556799+andreffrosa@users.noreply.github.com>
2022-05-19 14:03:53 +0100

    Clean code

--- main.js
+++ main.js
@@ -4,83 +4,109 @@ const {
     loadImage
 } = require("canvas");
 const fs = require('fs');
-const commandLineArgs = require('command-line-args');
+const processArgs = require('command-line-args');
 
-const optionDefinitions = [
-  { name: 'verbose', alias: 'v', type: Boolean, defaultValue: false },
-  { name: 'data', alias: 'd', type: String },
-  { name: 'out', alias: 'o', type: String },
-  { name: 'logo', alias: 'l', type: String, defaultValue: './logos/sni-logo2.png' },
-  { name: 'color', alias: 'c', type: String, defaultValue: '#3ba859' },
-  { name: 'size', alias: 's', type: Number, defaultValue: 720 }
+const ArgsDefinitions = [
+    {
+        name: 'verbose',
+        alias: 'v',
+        type: Boolean,
+        defaultValue: false
+    },
+    {
+        name: 'data',
+        alias: 'd',
+        type: String
+    },
+    {
+        name: 'out',
+        alias: 'o',
+        type: String
+    },
+    {
+        name: 'logo',
+        alias: 'l',
+        type: String,
+        defaultValue: './logos/logo.png'
+    },
+    {
+        name: 'color',
+        alias: 'c',
+        type: String,
+        defaultValue: '#000'
+    },
+    {
+        name: 'size',
+        alias: 's',
+        type: Number,
+        defaultValue: 720
+    }
 ];
 
-async function genQRCode(options) {
-
-    const {verbose, data, logo, out, color, size} = options;
-
-    const canvas = createCanvas(size, size);
-
-    let p = new Promise((resolve, reject) => {
+async function drawQRCode(canvas, data, size, color) {
+    return new Promise((resolve, reject) => {
         QRCode.toCanvas(
             canvas,
-            data,
-            {
+            data, {
                 errorCorrectionLevel: 'H',
                 margin: 1,
                 width: size,
                 height: size,
                 color: {
-                    //dark: "#000000",
                     dark: color,
                     light: '#0000' // Transparent background
                 },
             },
-        (err) => {
-            if (err) reject(err);
+            (err) => {
+                if (err) reject(err);
 
-            resolve();
-        }
+                resolve();
+            }
         );
     });
+}
 
-    await p;
-
-
-    const ctx = canvas.getContext("2d");
-
-    const logo_size = size*0.37;
-    const pos = ((size - logo_size) / 2);
-
-    //ctx.save();
-    //ctx.globalCompositeOperation = 'destination-out';
-    //ctx.beginPath();
-    //ctx.arc(x, y, radius, 0, 2 * Math.PI, false);
-    //ctx.fillRect(pos, pos, logo_size, logo_size);
-    //ctx.restore();
+async function drawLogo(canvas, logo, ratio = 0.37) {
+    const logo_size = size * ratio;
+    const pos = (size - logo_size) / 2;
 
     const img = await loadImage(logo);
 
+    const ctx = canvas.getContext("2d");
     ctx.clearRect(pos, pos, logo_size, logo_size);
     ctx.drawImage(img, pos, pos, logo_size, logo_size);
+}
 
-    //return canvas.toDataURL("image/png");
-    /*const out = fs.createWriteStream(output);
-    const stream = canvas.createPNGStream({resolution: 500});
-    stream.pipe(out);
-    out.on('finish', () => console.log('The PNG file was created.'));*/
+async function genQRCode(options) {
 
+    const {
+        verbose,
+        data,
+        logo,
+        out,
+        color,
+        size
+    } = options;
+
+    const canvas = createCanvas(size, size);
+
+    await drawQRCode(canvas, data, size, color);
+
+    await drawLogo(canvas, logo);
+
+    // Save to .png
     const buffer = canvas.toBuffer('image/png');
     fs.writeFileSync(out, buffer);
 
-    if(verbose)
-        console.log(`out: ${out} size: ${size} logo_size: ${logo_size} pos: ${pos} data: ${data}`);
+    if (verbose)
+        console.log(`out: ${out} size: ${size} pos: ${pos} data: ${data}`);
 }
 
 async function main() {
-    const options = commandLineArgs(optionDefinitions);
+    // Process Args
+    const args = processArgs(ArgsDefinitions);
 
-    await genQRCode(options);
+    await genQRCode(args);
 }
 
 main();
